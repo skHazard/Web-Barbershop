b610c105bb0f9de7ec976ac1f4ba174aa4480826
skHazard <hazard.sk@gmail.com>
2017-02-21 23:03:35 +0200

    fighting with webpack for sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,39 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: './src/index.js',
+    entry: './src/index',
 
-  output: {
-    filename: 'bundle.js',
-    publicPath: ''
-  },
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/src/'
+    },
 
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    watch: NODE_ENV == 'development',
+
+    watchOptions: {
+        aggregateTimeout: 300
+    },
+
+    devtool: NODE_ENV == 'development' ? 'source-map' : null,
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /\/node_modules\//,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader!css-loader!autoprefixer-loader?browsers = last 2 versions!sass-loader')
+            }
+        ]
+    },
+    plugins : [
+        new ExtractTextPlugin('./style/main.css')
     ]
-  }
-}
+};
